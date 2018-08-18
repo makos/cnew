04f3646cdfb93ddd8e75a59cac77df04306de630
Mateusz Makowski <matmakos@gmail.com>
2018-08-18 10:17:54 +0200

    Work on paths

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env::args;
 use std::fs;
 use std::io;
-use std::path;
+use std::path::Path;
 use std::process::{exit, Command};
 
 fn main() {
@@ -10,29 +10,29 @@ fn main() {
         eprintln!("Not enough arguments. Use {} -h for help.", args[0]);
         exit(0);
     }
-    let mut dir_name: String = String::new();
+    let mut dir_name: &str = "";
     // let mut dir_path: path::Path::new(".");
     for arg in &args {
         match &arg[..] {
             "-h" => show_help(&args[0]),
             "-v" => version_info(),
-            name @ _ => dir_name = name.to_owned(),
+            name @ _ => dir_name = name,
         }
     }
-    build_dirs(&dir_name);
+    let src_dir = dir_name.to_owned() + "/src";
+    let inc_dir = dir_name.to_owned() + "/include";
+    let src_path = Path::new(&src_dir);
+    let inc_path = Path::new(&inc_dir);
+
+    let paths = vec![src_path, inc_path];
+    build_dirs(&paths);
     populate_dirs();
     git_init();
 }
 
-fn build_dirs(dir_name: &String) {
-    let mut src_dir = dir_name.clone();
-    let mut inc_dir = dir_name.clone();
-    src_dir.push_str("/src");
-    inc_dir.push_str("/include");
-    let src_path = path::Path::new(&src_dir);
-    let inc_path = path::Path::new(&inc_dir);
-    fs::create_dir_all(src_path).expect("Error creating src dir");
-    fs::create_dir_all(inc_path).expect("Error creating inc dir");
+fn build_dirs(paths: &Vec<&Path>) {
+    fs::create_dir_all(paths[0]).expect("Error creating src dir");
+    fs::create_dir_all(paths[1]).expect("Error creating inc dir");
 }
 
 fn populate_dirs() {}
